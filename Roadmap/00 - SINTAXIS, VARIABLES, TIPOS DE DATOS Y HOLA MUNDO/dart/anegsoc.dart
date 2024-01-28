6387347faa7f21462d9a8ddb413a57b912e8d969
Acoidan Francisco Negrín Socorro <acoidanns@gmail.com>
2024-01-28 01:52:28 +0000

    #00 - Dart

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/dart/anegsoc.dart
@@ -0,0 +1,25 @@
+//Url del lenguaje = https://dart.dev
+/*Se pueden establecer comentarios
+de estas dos maneras segun sea necesario*/
+
+//Declaramos una variable
+var Variable1;
+//En dart tenemos dos formas de declarar constantes
+//La primera se incializa en compilacion
+const Constante1 = "";
+//La segunda se inicializa en ejecucion
+final Constante2 = "";
+
+/*Estos son los tipos de variables que tenemos en Dart, tambien podemos simplemente usar la palabra "var"
+y no tendriamos que definir el tipo de dato*/
+int Numero1 = 1;
+double Double1 = 1;
+String Texto1 = "";
+bool Booleano1 = true;
+dynamic Dinamico1;
+
+var Variable2;
+
+void main(){
+  print("¡Hola, Dart!");
+}
